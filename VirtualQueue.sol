139692c567ef7237bc0a1c2cc0be38fa7f78c5f6
Sachin George Thomas <sachq1@gmail.com>
2020-06-03 23:04:59 +0530

    add Queue contract

--- VirtualQueue.sol
+++ VirtualQueue.sol
@@ -1,42 +1,52 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.4.22 <0.7.0;
 
+import { Queue } from "./Queue.sol";
+
 contract VirtualQueue {
-  // Consumer who buys a Product from the Store
+
+  // Consumer who buys an Product from the Store
   struct Consumer {
-    string name;
-    uint8 age;
-    bool isAuthorised;
+    address _address;
+    uint8 _yob;
+    address _activeStore;
+    bool _isAuthorised;
   }
 
   // Consumer who buys an Product from the Store
   struct Store {
-    string storeName;
-    address[] activeQueue;
-    bool isActive;
+    address _address;
+    string _storeName;
+    Queue _activeQueue;
+    uint8 _queueLength;
+    bool _isActive;
   }
+  // To track _activeQueue (Queue) and Consumer
+  mapping(address => mapping(uint32 => Consumer)) storeQueueMap;
 
   struct Product {
-    string itemName;
-    uint64 price; // Save in eth
-    bool isAvailable;
+    string _itemName;
+    uint64 _price; // Save in eth
+    bool _isAvailable;
   }
 
-  // Used as Global Variable
+  // Global Variables
   uint64 ONE_ETH = 1 ether;
+  uint8 MAX_QUEUE = 3;
 
   // Manages Stores
   address manager;
 
-  // Waiting-list queue
-  address[] public waitingQueue;
+  // Waiting-list queue and Registered Users List
+  Consumer[] private waitingQueue;
+  Consumer[] private registeredConsumers;
 
   // General Store Items
   mapping(uint32 => Product) public products;
   uint32 totalProducts = 0;
 
   // Registered Stores by the Manager
-  mapping(address => Store) public stores;
+  mapping(uint32 => Store) public stores;
   uint32 totalStores = 0;
 
   // Actions ONLY Managers can perform:
@@ -71,24 +81,60 @@ contract VirtualQueue {
       _productPrice * ONE_ETH,
       true
     );
-    totalProducts += 1;
   }
 
   // Deletes Product at index
   function disabledProduct(uint32 index) public onlyManager {
-    products[index].isAvailable = false;
+    products[index]._isAvailable = false;
   }
 
   // Register new Store for Manager
-  function registerStore(address _store, string memory _storeName) public onlyManager {
+  function registerStore(address _storeAddress, string memory _storeName) public onlyManager {
     // Do nothing if Store is Available
-    if (stores[_store].isActive) return;
-    stores[_store] = Store(
+    if (stores[totalStores]._isActive) return;
+    stores[totalStores] = Store(
+      _storeAddress,
       _storeName,
-      new address[](0),
+      new Queue(),
+      0,
       true
     );
     totalStores += 1;
+  }
+
+  // Request Queue: User requests for a queue
+  function requestQueue(uint8 _yearOfBirth) public view {
+    // Check If User is already in the Waiting-Queue
+    require(canRegisterUser(msg.sender), "Already in Waiting-Queue");
+
+    // Check If User has minimum age of 18
+    require(_yearOfBirth <= 2002, "Not Eligible");
+
+    // Check if atleast one Store is Available
+    require(totalStores > 0, "No Store(s) Available to register");
+
+    // TODO: Check If User is already
+    // Condition to check if user address exist in any of the Store
+  }
+
+  // Finds the Store with the smallest Queue
+  function findShortestQueue() private view returns(address) {
+    uint32 leastQueue = 0;
+    uint32 storeIndex = 0;
+    for (uint32 i = 0; i < totalStores; i++) {
+      if (stores[i]._queueLength <= leastQueue) {
+        storeIndex = i;
+      }
+    }
+    return stores[storeIndex]._address;
+  }
+
+  function canRegisterUser(address userAddress) private view returns(bool) {
+    for (uint i = 0; i < waitingQueue.length; i++) {
+      if (waitingQueue[i]._address == userAddress) return false;
+    }
+    // If not in the Waiting-Queue
+    return true;
   }
 
 }
