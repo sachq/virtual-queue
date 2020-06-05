93407f6d2460f1e2ec4f3f6879559caacc00197e
Sachin George Thomas <sachq1@gmail.com>
2020-06-05 19:12:14 +0530

    Make Queue Solidity file seperate

---
+++ Queue.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.22 <0.7.0;
+
+contract Queue {
+	mapping(uint256 => address) queue;
+	uint256 first = 1;
+	uint256 last = 0;
+
+	function enqueue(address data) public {
+		last += 1;
+		queue[last] = data;
+	}
+
+	function dequeue() public returns (address data) {
+		require(last >= first, "Queue is Empty");
+		data = queue[first];
+		delete queue[first];
+		first += 1;
+	}
+}
